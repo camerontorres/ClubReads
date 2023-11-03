994f5cc2b18bdd1a31dbc5b1630742c04f0dc7b7
camerontorres <camerontorres759@gmail.com>
2023-11-03 19:29:22 -0400

    fixed connection bugs

--- config/database.js
+++ config/database.js
@@ -1,45 +1,27 @@
 const mongoose = require("mongoose");
 
-
-
-
-
-
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
-      /*useNewUrlParser: true,
+      useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-      ************
-      connect-mongo was uninstalled from npm and replaced with npm i connect-mongo@3. 
-      from reading stack overflow it appears the above code is not neccecary for the app to run but I am keeping it 
-      here in the event of needing to replace the current connect-mongo version
-      ************/
     });
-    
-    
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
-
-  
 };
+
 const gracefulShutdown = () => {
   mongoose.connection.close();
-    console.log('MongoDB connection closed.');
-    process.exit(0); // Exit the process after closing the connection
-  ;
+  console.log('MongoDB connection closed.');
+  process.exit(0);
 };
 
 // Listen for termination or interrupt signals
 process.on('SIGINT', gracefulShutdown);
 process.on('SIGTERM', gracefulShutdown);
 
-
-
-module.exports = connectDB 
+module.exports = connectDB;
